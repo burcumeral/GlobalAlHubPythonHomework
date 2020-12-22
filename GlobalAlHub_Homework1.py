8b5bbfc5a4113dba28982b550aeb9ff70b4960f0
Burcu Meral <76446489+burcumeral@users.noreply.github.com>
2020-12-22 23:00:46 +0300

    Add files via upload

---
+++ GlobalAlHub_Homework1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+x1,x2,x3,x4,x5 = input("Enter a five value: ").split()
+print("First value is {} and second value is {} and third value is {} and fourth value is {} and fifth value is {}".format(x1,x2,x3,x4,x5))
+print(type(x1),type(x2),type(x3),type(x4),type(x5))
